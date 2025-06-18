86f174ee9dc08c0f36e445438cb725391a198e23
wintage-sk <satheeshias212@gmail.com>
2025-06-18 12:31:09 +0530

    check server

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@ const express = require("express")
 const server= express()
 server.use((req, res)=>{
     res.end("created repository for divitinder")
+    console.log("connected successfully")
 })
 server.listen(3000)
